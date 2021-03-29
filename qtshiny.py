1c51988b54dad91d47a22b6c0cbbfbd98eac09f3
jackhanyuan <jackhanyuan@foxmail.com>
2021-03-30 04:33:57 +0800

    change wtf form style

--- qtshiny.py
+++ qtshiny.py
@@ -4,20 +4,20 @@ from wtforms import Form, StringField, PasswordField, BooleanField, validators
 # 定义注册表单form，使用flask-WTF表单系统
 class RegistrationForm(Form):
     # username，文本类型，必填项，最少4位，最多20位, 匹配单词字符(单词 数字 下划线)
-    username = StringField(u'Username', [validators.DataRequired(),
-                                         validators.Length(min=4, max=20, message=u"Username can only be 4-20 characters."),
-                                         validators.Regexp('^[\w]+$', message="Username can only contain words, numbers, underscores.")])
+    username = StringField(u'Username', [validators.DataRequired(u'Username can not be empty.'),
+                                         validators.Length(min=4, max=20, message=u'Username can only be 4-20 characters.'),
+                                         validators.Regexp('^[\w]+$', message="Username can only contain words, numbers, underscores.")], render_kw={'class': 'form-control', 'placeholder': u'Username'})
     # email，文本类型，必填项，最少6位，最多35位
-    email = StringField(u'Email', validators=[validators.DataRequired(message=u'Email can not be empty'),
-                                              validators.Length(6, 35),
-                                              validators.Email(message=u'Please enter a valid email address, such as: username@domain.com')])
+    email = StringField(u'Email', validators=[validators.DataRequired(message=u'Email can not be empty.'),
+                                              validators.Email(message=u'Invalid email, correct format: name@domain.com.')], render_kw={'class': 'form-control', 'placeholder': u'Email'})
     # password，密码类型，必填项
-    password = PasswordField(u'Password', [validators.DataRequired(message=u'Password can not be empty.')])
+    password = PasswordField(u'Password', [validators.DataRequired(message=u'Password can not be empty.'),
+                                           validators.Length(min=4, max=20, message=u'Password can only be 4-20 characters.')], render_kw={'class': 'form-control', 'placeholder': u'Password'})
     # confirm password，密码类型，必填项，必须与password值相同，负责返回错误消息message
     confirm = PasswordField(u'Repeat Password',
-                            [validators.DataRequired(), validators.EqualTo('password', message='Password must match.')])
+                            [validators.DataRequired(), validators.EqualTo('password', message='Password must match.')], render_kw={'class': 'form-control', 'placeholder': u'Repeat Password'})
     # I accept it，布尔类型，必填项，✔后值为True
-    accept_tos = BooleanField(u"I accept the site rules", [validators.DataRequired()])
+    # accept_tos = BooleanField(u'I accept the site <a href='https://www.findn.cn:5000'>rules</a>', [validators.DataRequired()])
 
 
 # 定义app类，每个对象对应app表中的一个记录
